3590256eaf3952a292f376e69916ff6c9ecebb15
Mavri <me@mavri.dev>
2024-05-06 14:46:35 +0300

    misc: changed folder name

---
+++ text-fetcher/src/constants.ts
@@ -0,0 +1,11 @@
+const Constants = {
+  ApiKey: '7f59af901d2d86f78a1fd60c1bf9426a',
+  QueryString:
+    '("Digital Transformation" OR "Digital Projects" OR "Digital Strategy" OR "Technology Adoption" OR "Digital Innovation" OR "Tech Projects" OR "E-Business" OR "Digital Change") ',
+  ItemsPerQuery: 100,
+  UpperLimit: 5900,
+  StartYear: 2024,
+  EndYear: 2024,
+};
+
+export default Constants;
